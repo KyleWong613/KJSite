3fc172539378f5a94cee8bb145be3fb92bf19abe
kaijengwong <kaijengwong@micron.com>
2023-04-19 21:46:02 +0800

    add a bored api

--- KJSite/Scripts/myjs.js
+++ KJSite/Scripts/myjs.js
@@ -1,7 +1,21 @@
-﻿var currentYear = new Date().getFullYear(); 
+﻿$(document).ready(function () {
+    //bored api
+    let boredapi = 'https://www.boredapi.com/api/activity'
+    let type = 'education'
+
+    $.ajax({
+        url: boredapi + "?type=" + type,
+        contentType: "application/json",
+        dataType: 'json',
+        success: function (boredthing) {
+            $('.bored-activity').html(boredthing.activity);
+         }
+    });
+});
+
+
+var currentYear = new Date().getFullYear(); 
 $('.currentyear').text(currentYear);
-
-
 
 //back to Homepage
 function backToHome()
