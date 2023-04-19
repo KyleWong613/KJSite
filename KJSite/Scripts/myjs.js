25add7a4ea033b658b9d1a0270dd0930a1efc143
kaijengwong <kaijengwong@micron.com>
2023-04-19 22:05:20 +0800

    add relax activity

--- KJSite/Scripts/myjs.js
+++ KJSite/Scripts/myjs.js
@@ -1,15 +1,25 @@
 ﻿$(document).ready(function () {
     //bored api
     let boredapi = 'https://www.boredapi.com/api/activity'
-    let type = 'education'
-
+    let education = 'education'
+    let relaxation = 'relaxation'
+    //education
     $.ajax({
-        url: boredapi + "?type=" + type,
+        url: boredapi + "?type=" + education,
         contentType: "application/json",
         dataType: 'json',
         success: function (boredthing) {
-            $('.bored-activity').html(boredthing.activity);
-         }
+            $('.education-activity').html(boredthing.activity);
+        }
+    });
+    //relax
+    $.ajax({
+        url: boredapi + "?type=" + relaxation,
+        contentType: "application/json",
+        dataType: 'json',
+        success: function (boredthing) {
+            $('.relax-activity').html(boredthing.activity);
+        }
     });
 });
 
